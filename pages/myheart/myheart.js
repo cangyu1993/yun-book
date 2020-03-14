9c99d8d66887f31054a533e29ca6bd9338d098e3
cangyu1993 <1765244720@qq.com>
2020-03-14 23:09:49 +0800

    新增图片裁剪

--- pages/myheart/myheart.js
+++ pages/myheart/myheart.js
@@ -5,11 +5,15 @@ Page({
 
   data: {
     userInfo: {},
-    booknums:""
+    booknums: "",
+    src: '',
+    width: 250,//宽度
+    height: 250,//高度
+    showCut:false,//裁剪框显示
   },
 
 
-  onLoad: function(options) {
+  onLoad: function (options) {
     wx.getUserInfo({
       success: (data) => {
         // console.log(data)
@@ -18,22 +22,57 @@ Page({
         })
       }
     }),
-    this.getData()
+      this.getData(),
+      this.cropper = this.selectComponent("#image-cropper")
+  },
+  //裁剪框初始化完成调用
+  cropperload(e) {
+    console.log("cropper初始化完成");
+  },
+  //图片加载完成
+  loadimage(e) {
+    console.log("图片加载完成", e.detail);
+    //重置图片角度、缩放、位置
+    this.cropper.imgReset();
+  },
+  //点击裁剪框阅览图片
+  clickcut(e) {
+    console.log(e.detail);
+    //点击裁剪框阅览图片
+    wx.previewImage({
+      current: e.detail.url, // 当前显示图片的http链接
+      urls: [e.detail.url] // 需要预览的图片http链接列表
+    })
+  },
+  //点击头像实现上传本地图片
+  chooseImage(){
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        this.setData({
+          src: res.tempFilePaths,
+          showCut: true
+        })
+      }
+    })
   },
   //获取收藏总数
-  getData(){
-    return new Promise((resolve,reject)=>{
-      fetch.get("/collection/total").then(res=>{
+  getData() {
+    return new Promise((resolve, reject) => {
+      fetch.get("/collection/total").then(res => {
         resolve()
         // console.log(res)
         this.setData({
-          booknums:res.data
+          booknums: res.data
         })
       })
     })
   },
   //跳转到收藏页面
-  goldHouse(){
+  goldHouse() {
     wx.navigateTo({
       url: '/pages/goldhouse/goldhouse',
     })
@@ -48,6 +87,6 @@ Page({
 
 
 
-  
+
 
 })
